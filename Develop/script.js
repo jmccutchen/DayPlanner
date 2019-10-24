5ee5f597e7da55f96b7169ec653cd54851aa8a57
John McCutchen <purplegrits@yahoo.com>
2019-10-23 23:41:49 -0400

    got date added and working with save icon, need to position properly

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+
+var now = moment().format("dddd, MMMM Do");
+$("#currentDay").text(now);
+
+// need a for loop to go through all time slots and run if statement that includes moment() isAfter then turn a color
+
+// var timeSlot= 
+
+});
