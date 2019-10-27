c8924d92f3daa9e47c966ea49d7fc5796b1ed285
John McCutchen <purplegrits@yahoo.com>
2019-10-26 21:18:34 -0400

    color changes added, only need local storage piece

--- Develop/script.js
+++ Develop/script.js
@@ -9,4 +9,52 @@ $("#currentDay").text(now);
 
 // var timeSlot= 
 
+
+var hourSlot = $(".hour").data("value")
+
+// $.each(hourSlot, function (key, value) {
+//    var nowHour = moment().format("HH");
+//     var timeSlot = $(this).data('value');
+//     if (nowHour < value) {
+//         $(this).css({"background-color": "gray"})
+//     } else if (nowHour > value) {
+//         $(this).css({"background-color": "green"})
+//     } else if (nowHour === value) {
+//         $(this).css({"background-color": "red"})
+//     }
+//     console.log(key+ ":"+ value);
+   
+// });   
+
+
+
+
+$(".hour").each( (key, element) => {
+     var nowHour = moment().format("HH");
+     var timeSlot = $(element).attr('data-value');
+    // var JSONstring = JSONstringify.timeSlot
+    //  array.push($(".hour").data("value"))
+
+     if (nowHour < timeSlot) {
+         $(element).siblings("textarea:first").css({"background-color": "gray"})
+     } 
+     
+     if (nowHour > timeSlot) {
+         $(element).siblings("textarea:first").css({"background-color": "green"})
+     } 
+     
+     if (nowHour === timeSlot) {
+         $(element).siblings("textarea:first").css({"background-color": "red"})
+     }
+     console.log(element);
+ 
+     console.log(timeSlot);
+     console.log(nowHour);
+ });
+
+//  $("i").on("click", () => {
+
+
+//  })
+
 });
